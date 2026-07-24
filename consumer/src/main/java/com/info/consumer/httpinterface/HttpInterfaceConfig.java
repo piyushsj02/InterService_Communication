189dde4fdf186931f28480066c25f97198323b25
Piyush Jadhav <piyushsj02@gmail.com>
2026-07-24 17:07:32 +0530

    HttpInterface added

--- consumer/src/main/java/com/info/consumer/httpinterface/HttpInterfaceConfig.java
+++ consumer/src/main/java/com/info/consumer/httpinterface/HttpInterfaceConfig.java
@@ -2,6 +2,9 @@ package com.info.consumer.httpinterface;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -9,6 +12,7 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class HttpInterfaceConfig {
 
+    @Primary
     @Bean
     public ProducerHttpInterface webClientHttpInterface() {
         WebClient webClient = WebClient.builder().baseUrl("http://localhost:8081").build();
@@ -18,5 +22,16 @@ public class HttpInterfaceConfig {
         ProducerHttpInterface service = factory.createClient(ProducerHttpInterface.class);
         return service;
     }
+
+    @Bean
+    public ProducerHttpInterface restClientHttpInterface() {
+        RestClient restClient = RestClient.builder().baseUrl("https://api.github.com/").build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+        ProducerHttpInterface service = factory.createClient(ProducerHttpInterface.class);
+        return service;
+    }
+
 
 }
